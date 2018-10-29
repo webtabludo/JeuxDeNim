aadb535a7a94e62481016b4230aeb6b1fda9ed0b
webtabludo <ludo@imac-de-ludo.local>
2018-10-29 12:05:23 +0100

    Presque finalité de la suppression des alumettes

--- JeuxDeNim/Controllers/FirstViewController.swift
+++ JeuxDeNim/Controllers/FirstViewController.swift
@@ -12,9 +12,8 @@ class FirstViewController: UIViewController {
 
     @IBOutlet weak var msgErrorLabel: UILabel!
     @IBOutlet weak var nbrLigneTextField: UITextField!
-    var choixNbreLigne = LogiqueAffichage()
-    var resultX = 0
-    var resultY = 0
+    
+    var nbrLigne = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,11 +22,17 @@ class FirstViewController: UIViewController {
 
     @IBAction func validerAction(_ sender: UIButton) {
 
+        
+        // Test si un nombre de ligne de type Int est rentré
         if let choixInt = Int(nbrLigneTextField.text!) {
-             resultX = choixNbreLigne.logiqueAffichageX(nombreDeLigne: choixInt)
-             resultY = choixNbreLigne.logiqueAffichageY(nombreDeLigne: choixInt)
-            performSegue(withIdentifier: "FirstSegue", sender: nil)
-   
+             nbrLigne = choixInt
+            // Test si ce nombre est bien inférieur à 200 alumettes
+            if choixInt <= 14 {
+                performSegue(withIdentifier: "FirstSegue", sender: nil)
+            } else {
+                msgErrorLabel.text = "14 lignes maximum !"
+            }
+            
         } else {
             msgErrorLabel.text = "Veuillez rentrer un nombre de ligne valide !" }
     }
@@ -37,8 +42,8 @@ class FirstViewController: UIViewController {
         
         if segue.identifier == "FirstSegue" {
             let destVc = segue.destination as! ViewController
-            destVc.coordX = resultX
-            destVc.coordY = resultY
+
+            destVc.lignes = nbrLigne
         }
     }
  
--- JeuxDeNim/Controllers/ViewController.swift
+++ JeuxDeNim/Controllers/ViewController.swift
@@ -16,23 +16,62 @@ class ViewController: UIViewController {
     @IBOutlet weak var lignTextField: UITextField!
     @IBOutlet weak var nbreAllumetteTextField: UITextField!
     
-    var coordX: Int = 0
-    var coordY: Int = 0
-    
+    var coordX: Int = 1
+    var coordY: Int = 32
+    var lignes: Int = 0
+    var pyramide = ""
     
     
     override func viewDidLoad() {
-       
         super.viewDidLoad()
+        
+        for i in 0...(lignes - 1) {
+        
+        pyramide.append(String(repeating: " ", count: coordY - i) + String(repeating: "l", count: coordX + (2 * i)) + String(repeating: " ", count: coordY - i) + "\n")
+        zoneTextView.text = pyramide
+            
+            // compte le nombre d'allumette
+//        let test = pyramide.components(separatedBy: "l")
+//        print(pyramide, test.count-1)
+        }
         
         
      
     }
 
     @IBAction func validerAction(_ sender: UIButton) {
-       
+        
+        // Trouver la ligne sélectioné ( 65 caractères par lignes)
+        // si ligne 3 alors (3-1) * 65 va au début de la ligne
+        if let nbrLigneSelect = Int(lignTextField.text!) {
+            if let nbreAluSelect = Int(nbreAllumetteTextField.text!) {
+            
+            // Il faut vérifier qu'il reste assez d'alumettes
+            let startLigne = pyramide.index(pyramide.startIndex, offsetBy: ((nbrLigneSelect-1) * 65) + (65 - ((nbrLigneSelect-1) * 2) + 1)/2)
+            let endligne = pyramide.index(pyramide.startIndex, offsetBy: nbreAluSelect + ((nbrLigneSelect-1) * 65) + (65 - ((nbrLigneSelect-1) * 2) + 1)/2)
+            let laLigne = pyramide[startLigne...endligne]
+            let lRestant = laLigne.components(separatedBy: "l")
+            print(lRestant.count)
+            
+                                        if nbreAluSelect <= lRestant.count {
+            
+                                                    // Revoir la formule mais la solution est la !
+                                                    for i in 1...nbreAluSelect {
+                
+                let start = pyramide.index(pyramide.startIndex, offsetBy: ((nbrLigneSelect-1) * 65) + (65 - ((nbrLigneSelect-1) * 2) - 1)/2)
+                let end = pyramide.index(pyramide.startIndex, offsetBy: i + ((nbrLigneSelect-1) * 65) + (65 - ((nbrLigneSelect-1) * 2) - 1)/2)
+                
+            pyramide.replaceSubrange(start...end, with: " ")
+            //print(pyramide)
+            zoneTextView.text = pyramide
+            
+                                                                                }
+        // Faire jouer l'ordinateur
+                                        } else {
+                                            messageLabel.text = "Il ne reste pas assez d'alumettes"
+                }
+                                                                            }
+                                                                }
+                                                            }
 
-    }
-    
 }
-
